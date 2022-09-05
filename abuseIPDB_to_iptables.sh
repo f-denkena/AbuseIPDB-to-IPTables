8db29b7a7dc29fcae030734f1d85900c7b597de7
Federico Denkena <federico.denkena@posteo.de>
2022-09-05 18:11:57 +0200

    General improvements
    Usage of absolute fiel paths instead of relative ones. This ensures
    compatability once run as a cron job.
    Deletion of interface variable, due to weakened security. If left
    unspecified, all interfaces will be selected.

--- abuseIPDB_to_iptables.sh
+++ abuseIPDB_to_iptables.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 
-_pub_if=eth0        	    		    # Device which is connected to the internet
-_tempdb=abuseipdb.db   		 			# Name of temp file
-_abuseipdbList=abuseipdb-blacklist  	# Name of chain in iptables
-
-_countMinimum=15						# raw report count
-_maxAgeInDays=60						# determines how far back in time we go to fetch reports counted for the countMinimum parameter
-_confidenceMinimum=90					# confidence of abuse score
-_apiv2Key=yourKey						# abuseipdb.com APIv2 Key
+_tempdb=/tmp/abuseipdb.db		# Name of temp file
+_abuseipdbList=abuseipdb-blacklist	# Name of chain in iptables
+_countMinimum=15			# Minimum report count
+_maxAgeInDays=60			# Age of oldest report
+_confidenceMinimum=90			# Confidence of abuse - premium feature
+_apiv2Key=yourKey
 
 function setupBlacklist {
-	# Check if IPv4 chain exists
+	### Check if chain exists... ###
 	if $1 -n --list $_abuseipdbList > /dev/null
 	then
-		# if exists: flush it
+		### ...if it exists: flush it... ###
 		$1 --flush $_abuseipdbList
 	else
-		# else: create it
+		### ...else: create it. ###
 		echo "Create..."
 		$1 -N $_abuseipdbList
 		$1 -I INPUT -j $_abuseipdbList
-		$1 -I OUTPUT -j $_abuseipdbList
-		$1 -I FORWARD -j $_abuseipdbList
 	fi
 }
 
-# Get the abused IPs
-curl -G https://api.abuseipdb.com/api/v2/blacklist \
+### Get the IP blacklist. ###
+/usr/bin/curl -G https://api.abuseipdb.com/api/v2/blacklist \
 	-d countMinimum=$_countMinimum \
 	-d maxAgeInDays=$_maxAgeInDays \
 	-d confidenceMinimum=$_confidenceMinimum \
@@ -36,22 +32,28 @@ curl -G https://api.abuseipdb.com/api/v2/blacklist \
 		exit 1; 
 	}
 
-### Setup the blacklist ###
-setupBlacklist iptables
-setupBlacklist ip6tables
+### Log status. ###
+echo "Download successful."
+echo "Total: `cat $_tempdb | wc -l` IPs"
 
-# store each ip in temp file
+### Setup the blacklist ###
+setupBlacklist /sbin/iptables
+setupBlacklist /sbin/ip6tables
+
+### Loop through all listed IPs ###
 for ip in `cat $_tempdb`
 do
-	# Append everything to $_abuseipdbList
+	### Append to iptables. ###
 	if [[ $ip =~ .*:.* ]]
 	then
- 		ip6tables -A $_abuseipdbList -i ${_pub_if} -s $ip -j DROP
+ 		/sbin/ip6tables -A $_abuseipdbList -s $ip -j DROP
 	else
- 		iptables -A $_abuseipdbList -i ${_pub_if} -s $ip -j DROP
+ 		/sbin/iptables -A $_abuseipdbList -s $ip -j DROP
 	fi
 done
 
-# Delete temp file
-rm $_tempdb
+### Log status. ###
+echo "Local blacklist successfully updated."
+
 exit 0
+
